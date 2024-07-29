a656e18f61980d03df8feb67ff48aa47e4214b6f
andy.wen <andy.wen@ontariohealth.ca>
2024-07-29 13:09:16 -0400

    check in

--- firehose/filter.rb
+++ firehose/filter.rb
@@ -1,11 +1,4 @@
 def filter(event)
-    thorDebug = event.get('THOR_DEBUG')
-    if ( thorDebug != nil ) && ( 'true' == thorDebug.downcase ) 
-       puts JSON.pretty_generate( event )
-    end
-
-    # indexName = event.get('thorIndexName')
-    # event.set('[@metadata][target_index]', indexName )
-
+    puts JSON.pretty_generate( event )
     return [event]
 end    
